4f2b9c85a6efa795135cb130fb9e587d49b26339
Li Hong Zephan <lihongzephan@gmail.com>
2019-05-17 13:40:36 +0800

    bjproject most done

---
+++ nodejs/print.js
@@ -0,0 +1,101 @@
+﻿var fs = require('fs');
+
+// const escpos = require('escpos');
+
+// // Select the adapter based on your printer type
+// const device  = new escpos.USB();
+// // const device  = new escpos.Network('localhost');
+// // const device  = new escpos.Serial('/dev/usb/lp0');
+
+// const options = { encoding: "GB18030" /* default */ }
+// // encoding is optional
+
+// const printer = new escpos.Printer(device, options);
+
+// Printer
+// var PDFDocument = require("pdfkit");
+var printer = require('printer');
+var tmpData = '';
+
+function sendPrint2() {
+    console.log('send print start.');
+    tmpData = fs.readFileSync('./output.pdf');
+    // setTimeout(sendPrint2, 2000);
+    sendPrint2();
+}
+
+//sendPrint();
+function sendPrint() {
+    // let tmpData2 = tmpData;
+    console.log(printer.getSupportedPrintFormats());
+    printer.printDirect({
+        data: fs.readFileSync('./output.pdf'),
+        printer: 'hp100_raspbian',
+        type: 'PDF',
+        success: function (jobID) {
+            console.log("Print ID: " + jobID);
+        },
+        error: function(err){
+            console.log('print module err: ' + err);
+        }
+    });
+}
+
+var path = './test.txt';
+var bolShouldCheck = true;
+
+function funLoop() {
+    try {
+        if(bolShouldCheck) {
+            // Check file exist
+            if (fs.existsSync(path)) {
+                bolShouldCheck = false;
+
+                // Can print
+                sendPrint();
+
+                // Delete file
+                fs.unlinkSync(path);
+
+                setTimeout(function() {
+                    bolShouldCheck = true;
+                }, 5000);
+            }
+            setTimeout(funLoop, 5000);
+        }
+    } catch (err) {
+        // Nothing to do
+    }
+}
+
+//funLoop();
+// setInterval(funLoop, 5000);
+// sendPrint();
+
+console.log(printer.getSupportedPrintFormats());
+printer.printDirect({
+    data: fs.readFileSync('./output.pdf'),
+    printer: 'hp100_raspbian',
+    type: 'PDF',
+    success: function (jobID) {
+        console.log("Print ID: " + jobID);
+    },
+    error: function(err){
+        console.log('print module err: ' + err);
+    }
+});
+
+// device.open(function(){
+//   printer
+//   .font('a')
+//   .align('ct')
+//   .style('bu')
+//   .size(1, 1)
+//   .text('The quick brown fox jumps over the lazy dog')
+//   .text('敏捷的棕色狐狸跳过懒狗')
+//   .barcode('1234567', 'EAN8')
+//   .qrimage('https://github.com/song940/node-escpos', function(err){
+//     this.cut();
+//     this.close();
+//   });
+// });
---
+++ nodejs/print2.js
@@ -0,0 +1,50 @@
+﻿var printer = require('printer');
+var Canvas = require('canvas')
+var Image = Canvas.Image
+var canvas = new Canvas(2100, 2970)
+var ctx = canvas.getContext('2d');
+var fs = require('fs');
+var out = fs.createWriteStream('./output.png');
+var stream = canvas.pngStream();
+// var stream = canvas.jpegStream({
+//    bufsize: 4096, // output buffer size in bytes, default: 4096
+//    quality: 100 // JPEG quality (0-100) default: 75
+//   , progressive: false // true for progressive compression, default: false
+// });
+
+ctx.font = '50px Deng';
+ctx.beginPath();
+ctx.rect(0, 0, 2100, 2970);
+ctx.fillStyle = "white";
+ctx.fill();
+ctx.fillStyle = "black";
+ctx.fillText("Line 1: To Zephan:", 50, 50);
+ctx.fillText("Line 2: 我现在将 Text 改为 png", 50, 150);
+ctx.fillText("Line 3: 然后用 printer.printFile 打印 ./output.png", 50, 250);
+ctx.fillText("Line 4: 打印 png 档案从来没有失败过！", 50, 350);
+ctx.fillText("Line 5: 请看 print2.js 你就明白了", 50, 450);
+ctx.fillText("Line 6: From Ken", 50, 550);
+
+// stream.pipe(out);
+
+stream.on('data', function(chunk){
+  out.write(chunk);
+});
+
+stream.on('end', function(){
+  console.log('saved png');
+  setTimeout(funStartPrint,2000);
+});
+
+function funStartPrint() {
+    printer.printFile({
+        filename: './output.png',
+        printer: 'hp100_raspbian',
+        success: function (jobID) {
+            console.log("Print ID: " + jobID);
+        },
+        error: function(err){
+            console.log('print module err: ' + err);
+        }
+      });    
+}
